209db1859ed6f6ceb24f34584cc317663f41d3f4
jdrhea <93232701+jdrhea@users.noreply.github.com>
2025-11-30 20:56:32 -0600

    made keys

--- pypiano.py
+++ pypiano.py
@@ -7,19 +7,28 @@ pygame.mixer.init()
 GAME_WIDTH = 1300
 GAME_HEIGHT = 600
 
-KEY_WIDTH = 50
-KEY_HEIGHT = 200
+SPACE_WIDTH = 10
+
+KEY_WIDTH = (GAME_WIDTH / 14)-SPACE_WIDTH
+KEY_HEIGHT = 250
+
+keyLabels = {"C1", "D1", "E1", "F1", "G1", "A1", "B1", "C2", "D2", "E2", "F2", "G2", "A2", "B2"}
+
 
 pygame.init()    
 window = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
 pygame.display.set_caption('Pygame Piano')
 clock = pygame.time.Clock()
 
-key = pygame.Rect(0, GAME_HEIGHT - KEY_HEIGHT, KEY_WIDTH, KEY_HEIGHT)
+
 
 def draw():
-    for i in range(1, 13):
-        pygame.draw.rect(window, (255, 255, 255), key)
+    for i in range(0, 14):
+        key = pygame.Rect(0 + (KEY_WIDTH + SPACE_WIDTH) * i , GAME_HEIGHT - KEY_HEIGHT, KEY_WIDTH, KEY_HEIGHT)
+        pygame.draw.rect(window, (255, 255, 255), key)  # key fill
+        key_font = pygame.font.SysFont('Gill Sans', 25)
+        key_text = key_font.render("C", True, (175, 175, 175))
+        window.blit(key_text, (i * (KEY_WIDTH + SPACE_WIDTH) + KEY_WIDTH/2, KEY_HEIGHT *2))
 
 while True: # game loop
     for event in pygame.event.get():
